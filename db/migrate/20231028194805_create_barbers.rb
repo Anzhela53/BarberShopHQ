9bee15ab16a74405760b86cb706dc93c370fa247
Anzhela53 <a.y.golikova@gmail.com>
2023-10-28 22:59:21 +0300

    add: add Barbers migration

---
+++ db/migrate/20231028194805_create_barbers.rb
@@ -0,0 +1,12 @@
+class CreateBarbers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :barbers do |t|
+      t.text :name
+
+      t.timestamps
+    end
+    Barber.create :name => 'Jassie Pinkman'
+    Barber.create :name => 'Walter White'
+    Barber.create :name => 'Gus Fring'
+  end
+end
