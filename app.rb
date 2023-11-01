8edd755c272824ec06af70f864e783729517dc64
Anzhela53 <a.y.golikova@gmail.com>
2023-11-01 19:10:12 +0300

    add: save client information the lame way

--- app.rb
+++ app.rb
@@ -13,8 +13,34 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.order "created_at DESC"
+end
 
 get '/' do
-	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber
+	c.color = @color
+	c.save
+	erb "<h2>Thank you, #{@username}.
+			<h3>We're waiting for you: #{@datetime}.</h3> 
+				<p>Your barber: #{@barber}.</p>"
 end
